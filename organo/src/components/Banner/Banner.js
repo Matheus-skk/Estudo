438862896df40910d29317434af9e52f5a44c95a
Matheus de Sousa Silva <matheusmth110@gmail.com>
2025-06-05 14:30:49 -0300

    banner

---
+++ organo/src/components/Banner/Banner.js
@@ -0,0 +1,8 @@
+import './Banner.css'
+export default function Banner(){
+    return(
+        <header className='banner'>
+        <img src="/imagens/banner.png" alt="O banner principal da pagina do Organo"/>
+        </header>
+    )
+}
