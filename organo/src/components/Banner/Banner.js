c4d9552c5fdbf576fa254baf9f07946e74fbb7fb
Matheus Sousa <85310392+Matheus-skk@users.noreply.github.com>
2025-06-10 08:40:02 -0300

    Delete organo directory

--- organo/src/components/Banner/Banner.js
+++
@@ -1,8 +0,0 @@
-import './Banner.css'
-export default function Banner(){
-    return(
-        <header className='banner'>
-        <img src="/imagens/banner.png" alt="O banner principal da pagina do Organo"/>
-        </header>
-    )
-}
--- organo/src/components/Button/index.js
+++
@@ -1,8 +0,0 @@
-import './Button.css'
-export default function Button(props){
-    return(
-        <div>
-            <button className='button'>{props.children}</button>
-        </div>
-    )
-}
