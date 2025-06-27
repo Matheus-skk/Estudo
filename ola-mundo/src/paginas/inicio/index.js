fd75ee92ec32a400521faa3c790f102834029513
Matheus de Sousa Silva <matheusmth110@gmail.com>
2025-06-27 10:14:25 -0300

    rota inicio

--- ola-mundo/src/paginas/inicio/index.js
+++ ola-mundo/src/paginas/inicio/index.js
@@ -1,7 +1,14 @@
+import Post from "components/Post";
+import styles from "./Inicio.module.css"
+import posts from "json/posts.json"
 export default function Inicio(){
     return(
-        <div>
-            <h1>Inicio</h1>
-        </div>
+            <ul className={styles.posts}>
+            {posts.map((post) => ( 
+                <li key={post.id}>
+                <Post post={post}/>
+                </li>))}
+            </ul>
+
     )
 }
